cd7bb036d17e8f892d0a67420143c53463def5ff
Katsuhide Tsuruta <katsuhide.tsuruta@nulab.com>
2024-01-25 23:12:45 +0900

    Initial Commit

---
+++ timesx-app/timesx_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  timesx_appApp.swift
+//  timesx-app
+//
+//  Created by Katsuhide Tsuruta on 2024/01/25.
+//
+
+import SwiftUI
+
+@main
+struct timesx_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ timesx-appUITests/timesx_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  timesx_appUITestsLaunchTests.swift
+//  timesx-appUITests
+//
+//  Created by Katsuhide Tsuruta on 2024/01/25.
+//
+
+import XCTest
+
+final class timesx_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
